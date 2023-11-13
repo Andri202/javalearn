e14999379a8a0a4e300d009f537009de8fbc6d46
andri202 <andrign20@gmail,com>
2023-11-13 09:32:39 +0700

    add fundamental code

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 // class name must match with file name
+/* multiline of code */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        String name = "andri";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        char myChar = 'A';
+        System.out.print(myChar);
+
+        // final is use if you not want other to override the value, it is like constant
+        final int myFinal = 2020;
+        System.out.println(myFinal);
+
     }
 }
